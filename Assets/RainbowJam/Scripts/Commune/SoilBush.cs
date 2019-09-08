29875804ad80515acc86361c0e3aeac9fb02d2d1
drmel <drmelon.mail@gmail.com>
2019-09-08 17:04:11 +0100

    Ambience not playing in city fix

--- Assets/RainbowJam/Scripts/Commune/SoilBush.cs
+++ Assets/RainbowJam/Scripts/Commune/SoilBush.cs
@@ -56,6 +56,7 @@ public class SoilBush : MonoBehaviour
 				Bush.transform.localScale = Vector3.one * progress;
 				if ( Time.time - StageTime >= BushGrowthTime )
 				{
+					Bush.transform.localScale = Vector3.one * progress;
 					StartBerries();
 				}
 
