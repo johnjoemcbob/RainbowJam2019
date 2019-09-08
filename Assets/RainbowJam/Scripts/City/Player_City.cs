952ffc2803f564f179f15fd637da97d30f3b411c
Matthew <mcormack1994@hotmail.co.uk>
2019-09-08 10:16:10 +0100

    Move city player to prefab, fix blank and add animations

--- Assets/RainbowJam/Scripts/City/Player_City.cs
+++ Assets/RainbowJam/Scripts/City/Player_City.cs
@@ -6,18 +6,28 @@ public class Player_City : MonoBehaviour
 {
     public float speed;
 
-    // Start is called before the first frame update
+	[Header( "References" )]
+	public GameObject IdleSprite;
+	public GameObject WalkingSprite;
+
+	protected Vector3 BaseScale;
+
     void Start()
     {
-        
-    }
+		BaseScale = WalkingSprite.transform.localScale;
+	}
 
-    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal") * speed * 0.5f;
         float vertical = Input.GetAxis("Vertical") * speed;
 
         transform.Translate(-horizontal, 0, -vertical);
-    }
+
+		// Animation for player
+		bool moving = ( horizontal + vertical ) != 0;
+		IdleSprite.SetActive( !moving );
+		WalkingSprite.SetActive( moving );
+		WalkingSprite.transform.localScale = new Vector3( BaseScale.x * ( horizontal >= 0 ? 1 : -1 ), BaseScale.y, BaseScale.z );
+	}
 }
