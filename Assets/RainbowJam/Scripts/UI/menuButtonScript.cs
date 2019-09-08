7f39f55df1d782468a2e75fc96195cb95ff7ba47
Caspar (Desktop) <caspar.vee@gmail.com>
2019-09-08 14:05:09 +0100

    Basic main menu + credits
    Just some simple canvases and buttons so that at least we have Something

---
+++ Assets/RainbowJam/Scripts/UI/menuButtonScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuButtonScript : MonoBehaviour
+{
+    public GameObject mainCanvas;
+    public GameObject creditCanvas;
+
+
+    // ------------------------------------------
+    void Start()
+    {
+        if (mainCanvas != null)
+        {
+            mainCanvas.SetActive(true);
+        }
+
+        if (creditCanvas != null)
+        {
+            creditCanvas.SetActive(false);
+        }
+    }
+
+
+    // ------------------------------------------
+    public void OnPlayClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // ------------------------------------------
+    public void OnCreditsClick()
+    {
+        if (mainCanvas != null)
+        {
+            mainCanvas.SetActive(false);
+        }
+
+        if (creditCanvas != null)
+        {
+            creditCanvas.SetActive(true);
+        }
+    }
+
+    // ------------------------------------------
+    public void OnCreditsReturnClick()
+    {
+        if (mainCanvas != null)
+        {
+            mainCanvas.SetActive(true);
+        }
+
+        if (creditCanvas != null)
+        {
+            creditCanvas.SetActive(false);
+        }
+    }
+}
